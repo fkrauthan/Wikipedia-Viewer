9de9e90124a973d306c1bbe20018d013325b0500
Florian Krauthan <mail@fkrauthan.de>
2015-07-09 19:17:40 -0700

    Added basic ajax system to save/un-save a favorite

--- symfony/src/AppBundle/Resources/public/js/search.js
+++ symfony/src/AppBundle/Resources/public/js/search.js
@@ -13,15 +13,33 @@ $('.search-results .list-group a').click(function(e) {
     e.preventDefault();
 
     var el = $(this);
+    var favoriteEl = el.find('.favorite');
 
     var url = el.data('url');
     var description = el.data('description');
     var title = el.data('title');
 
+    var unMarkUrl = favoriteEl.data('un-mark');
+    var markUrl = favoriteEl.data('mark');
+
     $('#result-title').text(title);
     $('#result-url').attr('href', url).text(url);
     $('#result-description').text(title);
     $('#result-iframe').attr('src', url);
+
+    var unMarkFavoriteEl = $('#un-mark-favorite');
+    var markFavoriteEl = $('#mark-favorite');
+    unMarkFavoriteEl.attr('href', unMarkUrl);
+    markFavoriteEl.attr('href', markUrl);
+
+    if(favoriteEl.hasClass('hide')) {
+        markFavoriteEl.removeClass('hide');
+        unMarkFavoriteEl.addClass('hide');
+    }
+    else {
+        markFavoriteEl.addClass('hide');
+        unMarkFavoriteEl.removeClass('hide');
+    }
 
     $('.search-results .list-group a').removeClass('active');
     el.addClass('active');
@@ -52,3 +70,29 @@ if(supportHistory) {
         }
     };
 }
+
+$('#un-mark-favorite').click(function(e) {
+    e.preventDefault();
+
+    var el = $(this);
+    var url = el.attr('href');
+    $.get(url, function() {
+        $('#un-mark-favorite').addClass('hide');
+        $('#mark-favorite').removeClass('hide');
+
+        $('.search-results .list-group a.active .favorite').addClass('hide');
+    });
+});
+
+$('#mark-favorite').click(function(e) {
+    e.preventDefault();
+
+    var el = $(this);
+    var url = el.attr('href');
+    $.get(url, function() {
+        $('#un-mark-favorite').removeClass('hide');
+        $('#mark-favorite').addClass('hide');
+
+        $('.search-results .list-group a.active .favorite').removeClass('hide');
+    });
+});
